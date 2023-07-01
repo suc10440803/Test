8ff31073d7f8e308d77fd61590799b984f4cd77d
Alex <suc1044080333@gmail.com>
2023-07-01 17:13:26 +0800

    change the score of first from 1550 to 1555

--- src/components/database.js
+++ src/components/database.js
@@ -1,7 +1,7 @@
 export const Leaderboard = [
     {
         name: "Shawn Hanna",
-        score : 1550,
+        score : 1555,
         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
         dt: "2022-02-10"
     },
