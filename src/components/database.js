8f3d019313e4d26f7704de555e2bfdd95d4e68e0
Alex <suc1044080333@gmail.com>
2023-07-12 07:49:12 +0800

    第二天分數更新

--- src/components/database.js
+++ src/components/database.js
@@ -1,27 +1,27 @@
 export const Leaderboard = [
     {
         name: "癡心絕隊",
-        score : 0,
+        score : 5900,
         img: "https://i.imgur.com/LgvNX3Qm.jpg"
     },
     {
         name: "不想肚子餓",
-        score : 0,
+        score : 4600,
         img: "https://i.imgur.com/rzgVGqdm.jpg"
     },
     {
         name: "三普跌倒",
-        score : 0,
+        score : 6450,
         img: "https://i.imgur.com/UNKa5Fhm.jpg"
     },
     {
         name: "喵喵最可愛",
-        score : 0,
+        score : 5700,
         img: "https://i.imgur.com/7WBqHJ5m.jpg"
     },
     {
         name: "嗚嗚嗚555",
-        score : 0,
+        score : 7450,
         img: "https://i.imgur.com/fbAVIR7m.jpg"
     }
 ]
