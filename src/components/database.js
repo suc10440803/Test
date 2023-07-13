53747a6223d2aabc2f55828c7c9e0866b9c12935
Alex <suc1044080333@gmail.com>
2023-07-13 20:57:04 +0800

    第四天分數更新

--- src/components/database.js
+++ src/components/database.js
@@ -1,27 +1,27 @@
 export const Leaderboard = [
     {
         name: "癡心絕隊",
-        score : 6700,
+        score : 21690,
         img: "https://i.imgur.com/LgvNX3Qm.jpg"
     },
     {
         name: "不想肚子餓",
-        score : 5100,
+        score : 19170,
         img: "https://i.imgur.com/rzgVGqdm.jpg"
     },
     {
         name: "三普跌倒",
-        score : 6700,
+        score : 23195,
         img: "https://i.imgur.com/UNKa5Fhm.jpg"
     },
     {
         name: "喵喵最可愛",
-        score : 6450,
+        score : 21375,
         img: "https://i.imgur.com/7WBqHJ5m.jpg"
     },
     {
         name: "嗚嗚嗚555",
-        score : 8100,
+        score : 23610,
         img: "https://i.imgur.com/fbAVIR7m.jpg"
     }
 ]
